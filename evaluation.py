cbfca6b88a3fd8e0cf3750d8bb4f0353d5a769b9
wenli7363 <cjy0415@qq.com>
2025-04-13 01:33:18 +0800

    通过evaluate，添加了代理

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,10 @@
 import evaluate
 from transformers.trainer_utils import EvalPrediction
+from constants import ID_TO_LABEL  # 确保从 constants.py 导入 ID_TO_LABEL
+
+import os
+os.environ["http_proxy"] = 'http://127.0.0.1:8080'
+os.environ['https_proxy'] = 'http://127.0.0.1:8080'
 
 metric_evaluator = evaluate.load("seqeval")
 
@@ -22,16 +27,18 @@ def compute_metrics(eval_predictions: EvalPrediction) -> dict[str, float]:
 
     # 将预测值和标签转换为字符串形式
     true_labels = [
-        [metric_evaluator.id2label[label] for label in label_seq if label != -100]
+        [ID_TO_LABEL[label] for label in label_seq if label != -100]
         for label_seq in labels
     ]
     pred_labels = [
-        [metric_evaluator.id2label[pred] for pred, label in zip(pred_seq, label_seq) if label != -100]
+        [ID_TO_LABEL[pred] for pred, label in zip(pred_seq, label_seq) if label != -100]
         for pred_seq, label_seq in zip(predictions, labels)
     ]
 
     # 计算指标
-    results = metric_evaluator.compute(predictions=pred_labels, references=true_labels)
+    results = metric_evaluator.compute(
+        predictions=pred_labels, references=true_labels, zero_division=0
+    )
 
     # 返回总体的 precision, recall 和 f1
     return {
